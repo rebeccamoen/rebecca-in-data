cbd737d15a5b8a64bee35025ec34d7afb876ffae
rebeccamoen <rebeccamoen>
2025-04-11 13:04:17 +0000

    movie analysis sql

--- sql/analysis_imdb.sql
+++ sql/analysis_imdb.sql
@@ -1,9 +1,18 @@
 -- Amount of Movies Watched
-SELECT movie_id, COUNT(*) AS num_watched
+SELECT COUNT(*) AS num_watched
 FROM dim_movie;
 
--- Average Rating
-SELECT
-  COUNT(*) AS movies_watched,
-  ROUND(AVG(imdb_rating), 1) AS avg_rating
+-- Average IMDb Rating
+SELECT  
+  ROUND(AVG(imdb_rating), 1) AS avg_rating,
+  COUNT(*) AS total_movies
 FROM dim_movie;
+
+-- Amount of Movies Watched by Genre
+SELECT 
+  g.genre,
+  COUNT(*) AS num_watched 
+FROM bridge_movie_genre bg
+JOIN dim_genre g ON bg.genre_id = g.genre_id
+GROUP BY g.genre
+ORDER BY num_watched DESC;

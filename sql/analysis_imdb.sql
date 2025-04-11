d21a02b5ddd3978f90a6692ed04e89056b2d6e93
rebeccamoen <rebeccamoen>
2025-04-11 12:53:24 +0000

    sql analysis testing

--- sql/analysis_imdb.sql
+++ sql/analysis_imdb.sql
@@ -1,6 +1,9 @@
 -- Amount of Movies Watched
-
 SELECT movie_id, COUNT(*) AS num_watched
-FROM dim_movie
-GROUP BY genre
-ORDER BY num_watched DESC;
+FROM dim_movie;
+
+-- Average Rating
+SELECT
+  COUNT(*) AS movies_watched,
+  ROUND(AVG(imdb_rating), 1) AS avg_rating
+FROM dim_movie;
